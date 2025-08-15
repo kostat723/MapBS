68f8cdc82a2d35f7b5638bb1c22ff031e618d2a2
Kostan <kostat604@gmail.com>
2025-08-15 15:21:31 +0300

    .

--- src/components/traces/list_traces.jsx
+++ src/components/traces/list_traces.jsx
@@ -2,10 +2,6 @@
 
 function List_traces () {
 
-    // const linkes = [
-    //     {href: 'http://10.100.28.5:1234/', target: "_blank", name: 'Traces CDR MSS'},
-    //     {href: 'http://10.100.28.5:9000/', target: "_blank", name: 'Traces MME'}
-    // ]
 
     return (
         <ul>
